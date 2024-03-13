0cfcdd5bbe31f95c1f884343e38f1bbac4882e31
23rd3n <serden.eranil@tum.de>
2024-03-13 18:18:26 +0100

    V1.0

---
+++ build/chess_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/pphauRos1_ws/src/chess_package
+# Build directory: /home/student/pphauRos1_ws/build/chess_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
